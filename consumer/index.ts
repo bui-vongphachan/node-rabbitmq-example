d2ceda1df2c2b23a7ca2fbe7a76dc12f9e3262a4
vongphachan~ <vongphachannukkalash@gmail.com>
2024-04-16 00:01:37 +0700

    manually acknowledge message

--- consumer/index.ts
+++ consumer/index.ts
@@ -2,21 +2,35 @@ import amqp, { ConsumeMessage } from "amqplib";
 
 (async () => {
   try {
-    const connection = await amqp.connect("amqp://admin:admin@localhost:3771");
+    const connection = await amqp.connect("amqp://admin:admin@localhost:5672");
     const channel = await connection.createChannel();
     const queue = "hello";
 
     channel.assertQueue(queue, { durable: false });
 
     console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
-    channel.consume(queue, handleMessage, { noAck: true });
+    channel.consume(queue, async (msg) => await handleMessage(msg, channel), {
+      noAck: false,
+    });
   } catch (error) {
     console.error(error);
   }
 })();
 
-function handleMessage(msg: ConsumeMessage | null): void {
+async function handleMessage(
+  msg: ConsumeMessage | null,
+  channel: amqp.Channel
+): Promise<void> {
   if (!msg) return;
 
   console.log(" [x] Received %s", msg.content.toString());
+
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      console.log(" [x] Done");
+      channel.ack(msg);
+
+      resolve(null);
+    }, 10000);
+  });
 }
--- publisher/index.ts
+++ publisher/index.ts
@@ -2,14 +2,16 @@ import * as amqp from "amqplib";
 
 (async () => {
   try {
-    const conn = await amqp.connect("amqp://admin:admin@localhost:3771");
-    
+    const conn = await amqp.connect("amqp://admin:admin@localhost:5672");
+
     const ch = await conn.createChannel();
     const q = "hello";
 
+    const massage = "Hello World!";
+
     await ch.assertQueue(q, { durable: false });
-    await ch.sendToQueue(q, Buffer.from("SUS MAH Hello World!"));
-    console.log(" [x] Sent 'Hello World!'");
+    await ch.sendToQueue(q, Buffer.from(massage));
+    console.log(` [x] Sent ${massage}`);
   } catch (err) {
     console.log(err);
   }
